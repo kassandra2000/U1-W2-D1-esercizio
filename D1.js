1188fa889240695515550748218f6a4272d349b6
kassandra <falsittakassy@gmsil.com>
2024-03-04 14:22:11 +0100

    first commit

---
+++ D1.js
@@ -0,0 +1,78 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+// JavaScript prevede cinque tipi di dati primitivi, numeri, stringhe, booleani, null e undefined
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let myName = "Kassandra"
+console.log(myName)
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+let number1 = 12
+let number2 = 20
+console.log("somma tra 12+20")
+console.log(number1+number2)
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+myName= "falsitta"
+console.log(myName)
+//se invece è una costante dà un errore
+const myName2= "Kassandra"
+// myName2="falsitta" (lo commento altrimenti mi da errore)
+console.log(myName2)
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+console.log("sottrazione tra numeri 4 e 12")
+console.log(4-x)
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let name1= "john"
+let name2= "John"
+
+console.log("dimostrare la diversità")
+let notEquality=(name1 !== name2) ? "si name1 e name2 sono diversi" : "no name1 e name2 non sono diversi(quindi sono uguali)"
+console.log(notEquality)
+
+let equality=(name1 === name2.toLowerCase()) ? "si, sono uguali con il lowercase di name2":"no, non sono uguali con il lowercase di name2"
+console.log(equality)
